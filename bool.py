7564522f7465d553c927a74c6e83cac53a3f87e4
Tanya <tanya.sanjuan@gmail.com>
2025-10-12 19:15:53 +0100

    definition of booleans

---
+++ bool.py
@@ -0,0 +1,5 @@
+# Boolean values
+# Boolean values can only be True or False
+print(10 > 9) # prints True, because 10 is greater than 9
+print(10 == 9) # prints False, because 10 is not equal to 9
+print(10 < 9) # prints False, because 10 is not less than 9
