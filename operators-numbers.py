addd910c06e3e9b1b112f187b45acd465a1d5c10
Tanya <tanya.sanjuan@gmail.com>
2025-10-12 12:38:17 +0100

    convert types and import random module

--- operators-numbers.py
+++ operators-numbers.py
@@ -29,3 +29,30 @@ print(list(y)) # prints [1, 2, 3, 4, 5]
 
 z = range(1, 10, 2) # range from 1 to 9 with a step of 2 (odd numbers)
 print(list(z)) # prints [1, 3, 5, 7, 9]
+
+# Convert Types
+# we can't convert complex number into another number type
+# we can't convert from string to int or float if the string contains a number
+# but if the string contains letters, it will give an error
+# for example: "5" is a string and cannot be converted to int or float
+# but "five" cannot be converted to int or float because it contains letters
+# x = "5" # string
+# y = int(x) # this will raise an error because x is a string that contains a number
+# print(y) # error: ValueError: invalid literal for int() with base 10
+
+# we can convert between different types, for example, from int to float and from float to int.
+x = 5 # integer
+y = 2.5 # float
+
+x = float(x) # convert int to float
+print(x) # prints 5.0
+
+# when converting from float to int, the decimal part is removed (not rounded)
+y = int(y) # convert float to int (removes the decimal part)
+print(y) # prints 2
+
+# random number
+# Python doesn't have random() function to generate a random number
+# but we can use the random module to generate random numbers
+import random
+print(random.randrange(1, 10)) # prints a random number between 1 and 9 (10 is excluded)
