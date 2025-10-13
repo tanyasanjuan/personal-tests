05e950d39982ccdab5556d544b5de8610edc7530
Tanya <tanya.sanjuan@gmail.com>
2025-10-13 19:32:44 +0100

    Creation of a list

---
+++ lists.py
@@ -0,0 +1,7 @@
+# Lists, are used to store multiple items in a single variable.
+# Lists are created using square brackets []
+# Lists are ordered, changeable, and allow duplicate values.
+
+# Create a list
+mylist = ["apple", "banana", "cherry"]
+print(mylist) # prints the list
