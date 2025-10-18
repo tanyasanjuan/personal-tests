2b6f3a810f04431710013a84dd7d5e28730598ba
Tanya <tanya.sanjuan@gmail.com>
2025-10-18 15:43:23 +0100

    looping using list comprehension

--- looplist.py
+++ looplist.py
@@ -20,3 +20,37 @@ for i in range(len(my_list)):
 # however it's easier to use the for loop directly on the list, as the 1st example shows.
 #for fruit in my_list:
 #   print(fruit)
+
+# We can also use a while loop to loop through a list
+my_list = ["banana", "manzana", "fresa"]
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i = i + 1
+# however it's easier to use the for loop directly on the list, as the 1st example shows.
+
+# Looping using list comprehension
+# List comprehension offers a shorter syntax 
+# when you want to create a new list based on the values of an existing list.
+my_list = ["banana", "manzana", "fresa"]
+[print(fruit) for fruit in my_list]
+# however it's easier to use the for loop directly on the list, as the 1st example shows.
+# Note: List comprehensions are generally used to create new lists,
+# so using them just for printing is not a common practice.
+# But it's included here to demonstrate the concept.
+
+# We can use the comprehension to loop when:
+# - We want to create a new list based on the existing list
+# - We want to apply a transformation to each element
+my_list = ["banana", "manzana", "fresa"]
+uppercased_fruits = [fruit.upper() for fruit in my_list]
+print(uppercased_fruits)  # Output: ['BANANA', 'MANZANA', 'FRESA']
+
+# - We want to filter elements based on a condition
+my_list = ["banana", "manzana", "fresa", "kiwi", "mango"]
+filtered_fruits = [fruit for fruit in my_list if "a" in fruit]
+print(filtered_fruits)  # Output: ['banana', 'manzana', 'fresa', 'mango']
+
+# to generate even numbers from 0 to 10
+even_numbers = [num for num in range(11) if num % 2 == 0]
+print(even_numbers)  # Output: [0, 2, 4, 6, 8, 10]
