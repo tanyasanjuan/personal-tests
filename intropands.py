f37750955cbd57c2c318c7b03da711ba1e12ce72
Tanya <tanya.sanjuan@gmail.com>
2025-10-26 14:39:55 +0000

    DataFrame, using loc

--- intropands.py
+++ intropands.py
@@ -3,6 +3,8 @@
 # Pandas refers to 'Panel Data' and 'Python Data Analysis'
 # 'pd' is the alias of Pandas, we can import an alias with the keyword 'as'
 # after name the alias, pandas ca be referred to as 'pd' instead o 'pandas'
+# Data sets in Pandas are usually multi-dimensional tables, called DataFrames
+# Series: is like a column, a DataFrame: is the whole table.
 # Source of Pandas' code: https://github.com/pandas-dev/pandas
 # This program loads a CVS file into a pandas DataFrame
 
@@ -39,6 +41,38 @@ print(mivariable)
 # results, instead to print the index (0, 1, 2) will print (x, y, z)
 # we can access an item by referring to the label.
 print(mivariable['z'])
+
+
+# Key/Value Objects as Series
+# creat a simple Pandas Series from a dictionary
+# The keys of the dictionary become the labels
+calorias = {"dia1": 420, "dia2": 380, "dia3":390}
+mivariable = pd.Series(calorias)
+print(mivariable)
+
+# to select only one of the imtem in the dictionary, we use the 'index' argument
+# and specify only the items we want to include on the Series
+# create a series using only data from 'dia1 and dia2'
+calorias = {"dia1": 420, "dia2": 380, "dia3": 390}
+mivariable = pd.Series(calorias, index = ["dia1", "dia2"])
+print(mivariable)
+
+# DataFrame: is a 2 dimensional data structure, like dimensional awway or a table with rows and columns
+# creat a DataFramce from two Series:
+data = {"calorias": [420, 380, 390],
+        "duracion": [50,40,45]
+        }
+mivariable = pd.DataFrame(data)
+print(mivariable)
+
+# Pandas use the 'loc' attribute to return one or more specified row(s)
+# refer to the rown index:
+print(mivariable.loc[0])
+
+# return row 0 and 1:
+# use a list of indexes:
+# when using [], the result is a Pandas DataFrame
+print(mivariable.loc[[0,1]])
 
 #df = pd.read_csv('dataprueba.csv')
 #print(df.to_string())
