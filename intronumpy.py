46f0b25edd66663566a1a5d3dec62f6df763e579
Tanya <tanya.sanjuan@gmail.com>
2025-10-28 20:54:49 +0000

    Using a tuple to create a numpy array

--- intronumpy.py
+++ intronumpy.py
@@ -6,7 +6,21 @@
 # https://github.com/numpy/numpy
 
 # NumPy is usually imported under the np alias.
+
+# NumPy Creating Arrays
+# we can create a NumPy ndarray object using the array() function.
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 #print(np.__version__)
+
+
+# type(): This built-in Python function tells us the type of the object passed to it. 
+# Results: shows that arr is numpy.ndarray type.
+print(type(arr))
+
+# Create an ndarray, we can pass a list, tuple or any array-like object into the array() method,
+# and it will be converted into an ndarray.
+# use a tuple to create a NumPy array:
+arr = np.array((1, 2, 3, 4, 5))
+print(arr)
