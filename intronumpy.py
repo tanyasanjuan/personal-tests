0c778b983147ef14f9defe4caabdbb02fb66510f
Tanya <tanya.sanjuan@gmail.com>
2025-10-28 20:45:44 +0000

    Import numpy, and print an array

---
+++ intronumpy.py
@@ -0,0 +1,12 @@
+# NumPy is a Python library used for working with arrays.
+# In Python we have lists that serve the purpose of arrays, but they are slow to process.
+# NumPy aims to provide an array object that is up to 50x faster than traditional Python lists.
+# The array object in NumPy is called ndarray,
+# it provides a lot of supporting functions that make working with ndarray very easy.
+# https://github.com/numpy/numpy
+
+# NumPy is usually imported under the np alias.
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+#print(np.__version__)
