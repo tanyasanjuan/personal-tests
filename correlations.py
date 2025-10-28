555b5e3865b873508d18b3384889b1e3eaa42318
Tanya <tanya.sanjuan@gmail.com>
2025-10-28 19:43:30 +0000

    Correlations, fiding relationships using the 'corr()' method.

---
+++ correlations.py
@@ -0,0 +1,32 @@
+# Data correlations
+# Finding Relationships, 
+# The corr() method calculates the relationship between each column in the data set.
+# The corr() method ignores "not numeric" columns.
+
+import pandas as pd
+df = pd.read_csv('datacorrelation.csv')
+print(df.corr())
+
+# The Result of the corr() method is a table with a lot of numbers 
+# that represents how well the relationship is between two columns.
+# The number varies from -1 to 1.
+    # 1 means that there is a 1 to 1 relationship (a perfect correlation), 
+    # and for this data set, each time a value went up in the first column, the other one went up as well.
+    # 0.9 is also a good relationship, and if you increase one value, the other will probably increase as well.
+    # -0.9 would be just as good relationship as 0.9, but if you increase one value, 
+    # the other will probably go down.
+    # 0.2 means NOT a good relationship, 
+    # meaning that if one value goes up does not mean that the other will.
+
+#In this dataset:   
+# Perfect correlation: "Duration" and "Duration" got the number 1.000000, 
+# which makes sense, each column always has a perfect relationship with itself.
+
+# Good Correlation: "Duration" and "Calories" got a 0.922721 correlation, 
+# which is a very good correlation, and we can predict that the longer you work out, the more calories you burn,
+# and the other way around: 
+# if you burned a lot of calories, you probably had a long work out.
+
+# Bad correlation: "Duration" and "Maxpulse" got a 0.009403 correlation, 
+# which is a very bad correlation, meaning that we can not predict the max pulse 
+# by just looking at the duration of the work out, and vice versa.
